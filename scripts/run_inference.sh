975ad143ea33dbaa60ed66047a6e3303205c68aa
Evangelos Ververas <evangelos.ververas@huawei.com>
2023-12-05 16:32:09 +0000

    Add scripts folder

--- scripts/run_inference.sh
+++ scripts/run_inference.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0 python inference.py \
                 --cfg configs/inference/inference.yaml \
-                --inference_data_file '/home/evangelosv/git_code/dense3Deyes/output/preprocessing/data_face68.pkl' \
-                --inference_dataset_dir '/storage/nfs2/evangelosv/databases/Face/AFLW/images/' \
+                --inference_data_file 'output/preprocessing/data_face68.pkl' \
+                --inference_dataset_dir 'data/example_images/' \
                 --checkpoint output/models/singleview/vertex/ALL/test_0/checkpoint.pth \
                 --skip_optimizer
